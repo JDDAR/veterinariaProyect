040557ca859ed9ba4dc6ce527cc1a5ebffc5d1b5
JDAAR <jdanaconar@gmail.com>
2024-12-06 07:39:07 -0500

    Estilos Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,6 +9,11 @@ const Home = () => {
       <HeaderNav />
       <div className="containerHome">
         <img src={BgImage} alt="" />
+        <div className="containerHome__contentHome">
+          <h2>Todo Para tu mascota</h2>
+          <h3>Descubre nuestras ofertas y servicios exclusivos</h3>
+          <NavLink to="/">Conoce mas</NavLink>
+        </div>
       </div>
     </>
   );
